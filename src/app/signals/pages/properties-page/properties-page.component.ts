f00c85fa810288b9041b314aa3e013f9ef15a802
Candela Landi <landicandela01@gmail.com>
2024-04-25 13:34:04 -0600

    project completed

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, computed, effect, signal } from '@angular/core';
 import { User } from '../../interfaces/user-request.interface';
 
 @Component({
@@ -6,19 +6,72 @@ import { User } from '../../interfaces/user-request.interface';
   templateUrl: './properties-page.component.html',
   styleUrl: './properties-page.component.scss'
 })
-export class PropertiesPageComponent {
-
-public user = signal<User>({
-  "id": 1,
-  "email": "george.bluth@reqres.in",
-   "first_name": "George",
-  "last_name": "Bluth",
-  "avatar": "https://reqres.in/img/faces/1-image.jpg",
-})
+export class PropertiesPageComponent implements OnDestroy, OnInit {
 
 
-  onFieldUpdated(field: string, value: string) {
-
-    console.log(field, value)
+  ngOnInit(): void {
+    setInterval(() => {
+      this.counter.update(current => current + 1);
+      if( this.counter() == 15 )
+        this.userChangedEffect.destroy()
+    }, 1000)
   }
+
+  public counter = signal(10);
+
+  public user = signal<User>({
+    "id": 1,
+    "email": "george.bluth@reqres.in",
+    "first_name": "George",
+    "last_name": "Bluth",
+    "avatar": "https://reqres.in/img/faces/1-image.jpg",
+  })
+
+  public fullName = computed(() => `${this.user().first_name} ${this.user().last_name}`)
+  public userChangedEffect = effect(() => {
+    console.log(`${this.user().first_name} - ${this.counter()}`)
+  })
+
+  onFieldUpdated(field: keyof User, value: string) {
+
+    /*     this.user.set({
+      ...this.user(),
+      [field]: value,
+    }) */
+
+    this.user.update(current => {
+
+      switch (field) {
+
+        case 'email':
+          current.email = value;
+          break;
+        case 'avatar':
+          current.avatar = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+        case 'id':
+          current.id = Number(value);
+
+      };
+
+      return current
+
+    });
+
+  }
+  incraseBy(value: number) {
+    this.counter.update(current => current + value);
+  }
+
+
+  ngOnDestroy(): void {
+
+  }
+
 }
